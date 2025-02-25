ac21946665e2a9192e1ac25a20ce6fd9ac5d6fd8
Henri Kim Pinheiro Rodrigues <hkimpr40@gmail.com>
2025-02-25 09:30:45 -0300

    adjust login script

--- front/static/js/login.js
+++ front/static/js/login.js
@@ -1,11 +1,25 @@
 "use strict";
 
+async function main() {
+    const response = await fetch('/api/login', {
+        method: 'POST',
+        credentials: 'include',
+    });
+    const data = await response.json();
+
+    if(data.message) {
+        if(data.message == "User is logged") window.location.href = "/controlPanel";
+    }
+}
+
 async function login() {
-    let username = document.getElementById("username");
-    let password = document.getElementById("password");
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let output = document.getElementById("output");
 
     const response = await fetch('/api/login', {
         method: 'POST',
+        headers: {'Content-Type': 'application/json'},
         credentials: 'include',
         body: JSON.stringify({ username, password })
     });
@@ -13,10 +27,17 @@ async function login() {
     const data = await response.json();
 
     if(data.error) {
+        output.style.color = "#ff0f0f";
+        output.innerHTML = data.error;
         if(data.error == "Invalid token") login();
         if(data.error == "Username and password are required");
     }
     if(data.message) {
-        if(data.message == "User is logged") windows.location.href = "/controlPanel";
+        output.style.color = "#1cc238"
+        output.innerHTML = data.message;
+        if(data.message == "User is logged") window.location.href = "/controlPanel";
+        if(data.message == "User logged successfully") window.location.href = "/controlPanel";
     }
 }
+
+main()
